a081f5b837e48fb22329a4bceb161ec0a99ee3ad
katieamazing <k.allen@live.com>
2018-09-11 16:29:45 -0400

    Add file with new bodies for a different sector

---
+++ bodies.js
@@ -0,0 +1,103 @@
+//https://www.orffenspace.com/swn/swn-toolbox/
+
+var reference = [
+  {
+    "System": "",
+    "Chartloc": "",
+    "Locations": [
+      {
+        "Name": "",
+        "Environment": "",
+        "TL": "",
+        "Description": ""
+      },
+    ]
+  }
+]
+
+var bodies = [
+  {
+    "System": "Rye",
+    "Chartloc": "0105",
+    "Locations": [
+      {
+        "Name": "Rye",
+        "Environment": "Flat, many alluvial plains, oxygen-rich biosphere and temperate climate.",
+        "TL": "3",
+        "Description": "Rye is the capital of the Liberty Combine, an agrarian isolationist autarky. Rye is sparsely populated, with small towns being the only concentrations of people. Leadership is highly distributed and slow to act. Despite having spread to another planet, there is almost no communication between the planets of Rye and Wheat. The planet has a small number of in-system space ships that are almost all owned by pirates who feel that piracy is in keeping with self-sufficiency. There exists a contrarian faction in the Precious Element group, which believes that Rye should quit piracy, join the sector economy, and sell its considerable mineral resources on the open market."
+      },
+      {
+        "Name": "Wheat",
+        "Environment": "Rocky, hilly, with an oxygen-lean biosphere and pleasantly warm temperatures.",
+        "TL": "2",
+        "Description": "Wheat is an agrarian society founded after spacefarers discovered Rye and sought to join the Liberty Combine. Wheat's leadership prides itself on a rigid interpretation and application of the isolationist autarky started on Rye. The denizens of Wheat are universally very poor and live a subsistence lifestyle. Visitors, even from Rye, are not usually welcome."
+      }
+    ]
+  },
+  {
+    "System": "Rasarc",
+    "Chartloc": "0403",
+    "Locations": [
+      {
+        "Name": "Rashart",
+        "Environment": "Oxygen-rich biosphere and temperate climate.",
+        "TL": "4",
+        "Description": "Rashart is the center of the Ran Empire. This lush homeworld is densely populated with sybaritic inhabitants. A high tech level and a deep and complex serf culture enable most residents of Rashart to pass their days in idleness. Rashart is an artistic and leisure capital, synonymous with luxury and excess."
+      },
+      {
+        "Name": "Raslom",
+        "Environment": "Oxygen-rich biosphere and temperate climate.",
+        "TL": "4",
+        "Description": "Named for its green appearance, Raslom produces a wide array of top-quality agricultural produce. The dynastic families that own the vast farms of Raslom are secretive, competitive, and cutthroat when it comes to securing the genetic and cultivation secrets of their holdings."
+      }
+    ]
+  },
+  {
+    "System": "Rassen",
+    "Chartloc": "0404",
+    "Locations": [
+      {
+        "Name": "Jewel",
+        "Environment": "Oxygen-rich biosphere and temperate climate.",
+        "TL": "4",
+        "Description": "Jewel is a lightly populated world conquered by the Ran Empire fairly recently. Theran construction is occuring all around, with some tension between old Theran families and new Theran entrepreneurs and investment groups desiring new farming land and space to build manses. There are rumors of an underground and long-surviving resistance from the conquered peoples of Jewel."
+      },
+      {
+        "Name": "Raspire",
+        "Environment": "Rocky, hilly, with an oxygen-lean biosphere and pleasantly warm temperatures.",
+        "TL": "0",
+        "Description": "Raspire is a rocky, uninhabited world. It boasts a wide variety of avian life, and infrequently hosts Ran adventurers and tourists."
+      }
+    ]
+  },
+  {
+    "System": "Shadowrunland",
+    "Chartloc": "0606",
+    "Locations": [
+      {
+        "Name": "Myev",
+        "Environment": "Cold temperatures, dry rocky ravines, freshwater oceans, very little plant life. The sun in this system produces lower than usual light. Normal breathable biosphere.",
+        "TL": "4",
+        "Description": "Myev is inhabited by a humanoid with some mutations to adapt the Myev's cool temperatures and dark environment. Myev has a strict government ruled by powerful corporations. Each Myvian is given a unique digit at birth in the form of a microchip that is used heavily to track their movements and habits. Strong security culture in most areas, but there are lawless pockets and some Unumbered."
+      },
+      {
+        "Name": "Koshk",
+        "Environment": "Very cold temperatures, a warming suit is required to be outdoors here. The sun in this system produces lower than usual light. Normal breathable biosphere.",
+        "TL": "3",
+        "Description": "Koshk is a heavy industry suburb of Myev. Many of the company compounds here work on projects that are too dangerous or restricted to develop on Myev. Most of the inhabitants are automated robots; transit in and out is restricted, and the data link between the two planets is minimal and managed carefully."
+      },
+      {
+        "Name": "Yanno",
+        "Environment": "Very cold temperatures, no biosphere. A warming suit and breathing apparatus are required to be outdoors here.",
+        "TL": "0",
+        "Description": "Unhinhabited rocky world."
+      },
+      {
+        "Name": "Solniki",
+        "Environment": "Warm temperatures, toxic biosphere. A breathing apparatus is required to be outdoors here.",
+        "TL": "0",
+        "Description": "Unhinhabited world full of alien fauna. Atmosphere is slightly corrosove over time. Solniki gets the most sunlight of any planet in this system."
+      },
+    ]
+  }
+]
