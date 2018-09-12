b011c73e65e01ccef92fc7118ff4023a33fb8e9b
Katie Amazing <k.allen@live.com>
2018-09-11 21:11:32 -0400

    Change order of planets to display correctly.

--- bodies.js
+++ bodies.js
@@ -18,20 +18,20 @@ var reference = [
 var bodies = [
   {
     "System": "Rye",
-    "Chartloc": "0105",
+    "Chartloc": "0108",
     "Locations": [
+      {
+        "Name": "Wheat",
+        "Environment": "Rocky, hilly, with an oxygen-lean biosphere and pleasantly warm temperatures.",
+        "TL": "2",
+        "Description": "Wheat is an agrarian society founded after spacefarers discovered Rye and sought to join the Liberty Combine. Wheat's leadership prides itself on a rigid interpretation and application of the isolationist autarky started on Rye. The denizens of Wheat are universally very poor and live a subsistence lifestyle. Visitors, even from Rye, are not usually welcome."
+      },
       {
         "Name": "Rye",
         "Environment": "Flat, many alluvial plains, oxygen-rich biosphere and temperate climate.",
         "TL": "3",
         "Description": "Rye is the capital of the Liberty Combine, an agrarian isolationist autarky. Rye is sparsely populated, with small towns being the only concentrations of people. Leadership is highly distributed and slow to act. Despite having spread to another planet, there is almost no communication between the planets of Rye and Wheat. The planet has a small number of in-system space ships that are almost all owned by pirates who feel that piracy is in keeping with self-sufficiency. There exists a contrarian faction in the Precious Element group, which believes that Rye should quit piracy, join the sector economy, and sell its considerable mineral resources on the open market."
       },
-      {
-        "Name": "Wheat",
-        "Environment": "Rocky, hilly, with an oxygen-lean biosphere and pleasantly warm temperatures.",
-        "TL": "2",
-        "Description": "Wheat is an agrarian society founded after spacefarers discovered Rye and sought to join the Liberty Combine. Wheat's leadership prides itself on a rigid interpretation and application of the isolationist autarky started on Rye. The denizens of Wheat are universally very poor and live a subsistence lifestyle. Visitors, even from Rye, are not usually welcome."
-      }
     ]
   },
   {
@@ -57,23 +57,30 @@ var bodies = [
     "Chartloc": "0404",
     "Locations": [
       {
+        "Name": "Raspire",
+        "Environment": "Rocky, hilly, with an oxygen-lean biosphere and pleasantly warm temperatures.",
+        "TL": "0",
+        "Description": "Raspire is a rocky, uninhabited world. It boasts a wide variety of avian life, and infrequently hosts Ran adventurers and tourists."
+      },
+      {
         "Name": "Jewel",
         "Environment": "Oxygen-rich biosphere and temperate climate.",
         "TL": "4",
         "Description": "Jewel is a lightly populated world conquered by the Ran Empire fairly recently. Theran construction is occuring all around, with some tension between old Theran families and new Theran entrepreneurs and investment groups desiring new farming land and space to build manses. There are rumors of an underground and long-surviving resistance from the conquered peoples of Jewel."
       },
-      {
-        "Name": "Raspire",
-        "Environment": "Rocky, hilly, with an oxygen-lean biosphere and pleasantly warm temperatures.",
-        "TL": "0",
-        "Description": "Raspire is a rocky, uninhabited world. It boasts a wide variety of avian life, and infrequently hosts Ran adventurers and tourists."
-      }
+
     ]
   },
   {
-    "System": "Shadowrunland",
+    "System": "Sonos",
     "Chartloc": "0606",
     "Locations": [
+      {
+        "Name": "Solniki",
+        "Environment": "Warm temperatures, toxic biosphere. A breathing apparatus is required to be outdoors here.",
+        "TL": "0",
+        "Description": "Unhinhabited world full of alien fauna. Atmosphere is slightly corrosove over time. Solniki gets the most sunlight of any planet in this system."
+      },
       {
         "Name": "Myev",
         "Environment": "Cold temperatures, dry rocky ravines, freshwater oceans, very little plant life. The sun in this system produces lower than usual light. Normal breathable biosphere.",
@@ -91,12 +98,6 @@ var bodies = [
         "Environment": "Very cold temperatures, no biosphere. A warming suit and breathing apparatus are required to be outdoors here.",
         "TL": "0",
         "Description": "Unhinhabited rocky world."
-      },
-      {
-        "Name": "Solniki",
-        "Environment": "Warm temperatures, toxic biosphere. A breathing apparatus is required to be outdoors here.",
-        "TL": "0",
-        "Description": "Unhinhabited world full of alien fauna. Atmosphere is slightly corrosove over time. Solniki gets the most sunlight of any planet in this system."
       },
     ]
   }
